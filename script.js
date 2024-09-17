d9d30ecb728f644a73a8786365104e8739a6c330
Igor <igorivandikov87@gmail.com>
2024-09-17 19:33:06 +0300

    modal window

---
+++ script.js
@@ -0,0 +1,7 @@
+// modal window
+
+document.querySelector('.menu-btn').addEventListener('click', function() {
+    document.querySelector('.modal-show').classList.toggle('show');
+});
+
+// modal window
