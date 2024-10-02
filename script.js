3ea491475fe93f6c9e4748a3bf8e24a4ca4138d9
Igor <igorivandikov87@gmail.com>
2024-10-02 22:22:30 +0300

    edits

--- script.js
+++ script.js
@@ -5,3 +5,13 @@
     });
 
 // modal window================================================================
+
+
+// scroll-up===================================================================
+
+document.getElementById('scrollToTop').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// scroll-up===================================================================
